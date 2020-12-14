b5a55212fc742406497d0b052d645d0b66a438ae
Guillermo Enrique Talavera <getalavera@outlook.com>
2020-12-14 18:20:06 -0500

    Agregando la funcion DESPEDIDA al archivo views.py

--- Django/proyDJ01/proyDJ01/urls.py
+++ Django/proyDJ01/proyDJ01/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from proyDJ01.views import saludo
+from proyDJ01.views import saludo, despedida
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('nosveremos/', despedida),
 ]
--- Django/proyDJ01/proyDJ01/views.py
+++ Django/proyDJ01/proyDJ01/views.py
@@ -4,4 +4,6 @@ def saludo(request):
 
     return HttpResponse('Hola alumnos esta es nuestra primera pagina con DJango')
 
+def despedida(request):
 
+    return HttpResponse('Hasta luego alumnos de DJango')
