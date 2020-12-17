1290da6e9fd6c9dc20a8e857d19f4b30d09a68d0
Guillermo Enrique Talavera <getalavera@outlook.com>
2020-12-16 23:57:19 -0500

    Creacion de plantilla saludo

--- Django/djProyecto01/djProyecto01/settings.py
+++ Django/djProyecto01/djProyecto01/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djProyecto01.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['D:/Local/Documentos/Programacion/GIT/Django/djProyecto01/djProyecto01/plantillas/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Django/djProyecto01/djProyecto01/vistas.py
+++ Django/djProyecto01/djProyecto01/vistas.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
-
+from django.template import Template, Context, loader
+from django.shortcuts import render
 
 def saludo(request):
 
-    msjSaludo = '<html><body><h1><p style="color:red;">Hola World!</p></h1></body></html>'
+    #msjSaludo = '<html><body><h1><p style="color:red;">Hola World!</p></h1></body></html>'
+
+    doc_externo = open('D:/Local/Documentos/Programacion/GIT/Django/djProyecto01/djProyecto01/plantillas/saludo.html', encoding='utf-8-sig')
+    # encoding='utf-8-sig' para eliminar simbolos raros al inicio de la Pagina Web
+
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+
+    doc_externo = loader.get_template('saludo.html')
+    ctx = Context()
+    msjSaludo = plt.render(ctx)
 
     return HttpResponse(msjSaludo)
 
